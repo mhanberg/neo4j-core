40b5f7e41f0f656d1594442954e94820ba486e2a
Mitchell Hanberg <mitchell.hanberg@bleacherreport.com>
2020-04-14 14:18:37 -0400

    Fall back to old behavior

--- lib/neo4j/core/cypher_session/adaptors/schema.rb
+++ lib/neo4j/core/cypher_session/adaptors/schema.rb
@@ -14,20 +14,48 @@ module Neo4j
             result = query(session, 'CALL db.indexes()', {}, skip_instrumentation: true)
 
             result.map do |row|
-              label = row.tokenNames.first.to_sym
-              properties = row.properties.map(&:to_sym)
-
-              {type: row.type.to_sym, label: label, properties: properties, state: row.state.to_sym}
+              {type: row[:type].to_sym, label: label(result, row), properties: properties(row), state: row[:state].to_sym}
             end
           end
 
           def constraints(session)
             result = query(session, 'CALL db.indexes()', {}, skip_instrumentation: true)
 
-            result.select { |row| row.type == 'node_unique_property' }.map do |row|
-              label, property = row.description.match(/INDEX ON :([^\(]+)\(([^\)]+)\)/)[1, 2]
-              {type: :uniqueness, label: label.to_sym, properties: [property.to_sym]}
+            result.select(&method(v4?(result) ? :v4_filter : :v3_filter)).map do |row|
+              {type: :uniqueness, label: label(result, row), properties: properties(row)}
             end
+          end
+
+          private
+
+          def v4_filter(row)
+            row[:uniqueness] == 'UNIQUE'
+          end
+
+          def v3_filter(row)
+            row[:type] == 'node_unique_property'
+          end
+
+          def label(result, row)
+            if v34?(result)
+              row[:label]
+            else
+              (v4?(result) ? row[:labelsOrTypes] : row[:tokenNames]).first
+            end.to_sym
+          end
+
+          def v4?(result)
+            return @v4 unless @v4.nil?
+            @v4 = result.columns.include?(:labelsOrTypes)
+          end
+
+          def v34?(result)
+            return @v34 unless @v34.nil?
+            @v34 = result.columns.include?(:label)
+          end
+
+          def properties(row)
+            row[:properties].map(&:to_sym)
           end
         end
       end
